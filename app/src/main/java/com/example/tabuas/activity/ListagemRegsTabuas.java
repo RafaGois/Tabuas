0ae21a6a7acdc413069454b51532ede7d93ee6ef
Rafael Fontana Becker Gois <87453597+RafaGois@users.noreply.github.com>
2022-10-08 11:15:02 -0300

    item listagem

--- app/src/main/java/com/example/tabuas/activity/ListagemRegsTabuas.java
+++ app/src/main/java/com/example/tabuas/activity/ListagemRegsTabuas.java
@@ -1,19 +1,30 @@
 package com.example.tabuas.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
 
 import com.example.tabuas.R;
+import com.example.tabuas.adapter.RegistroAdapter;
+import com.example.tabuas.helper.RegistroDAO;
+import com.example.tabuas.model.Registro;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class ListagemRegsTabuas extends AppCompatActivity {
 
     RecyclerView recyclerView;
+    private RegistroAdapter adapter;
+
+    private List<Registro> registros = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +34,8 @@ public class ListagemRegsTabuas extends AppCompatActivity {
         Objects.requireNonNull(getSupportActionBar()).hide();
 
         recyclerView = findViewById(R.id.recycler);
+
+        carregaListaRecycler();
     }
 
     @Override
@@ -34,10 +47,25 @@ public class ListagemRegsTabuas extends AppCompatActivity {
 
     public void navegaAdicao (View view) {
         Intent intent =  new Intent(this,AddRegTabuas.class);
+
         startActivity(intent);
     }
 
     public void volta (View view) {
         finish();
+    }
+
+    private void carregaListaRecycler () {
+        RegistroDAO dao = new RegistroDAO(getApplicationContext());
+
+        registros = dao.listar();
+
+        adapter =  new RegistroAdapter(registros);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.addItemDecoration(new DividerItemDecoration(getApplicationContext(), LinearLayout.VERTICAL));
+        recyclerView.setAdapter(adapter);
     }
 }
